fd2d182bbdafbd5f8bebb53096338ec97624db87
2301182 <2301182@s.asojuku.ac.jp>
2024-10-09 13:24:15 +0900

    a

---
+++ foodshop.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/foodshop.css" type="text/css">
+    <title>FoodShop</title>
+</head>
+<body>
+    <div id="menu_bar">
+        <img src="img/icon_65.png">
+    </div>
+</body>
+</html>
