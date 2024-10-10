9ee478b21876a8b6ac29ef999daf7965ac3acd4e
kouji1129 <2301232@s.asojuku.ac.jp>
2024-10-10 09:40:12 +0900

    ヘッダー

---
+++ home.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>トップ</title>
+    <style>
+    @import url(css/home.css);
+    </style>
+</head>
+<body>
+<header>
+    <div class="top">
+        <a href="#">
+        <img src="img/icon.png" alt="shop" while="50" height="50">
+        </a>
+    </div>
+
+    <div class="search">
+        <input type="search" placeholder="foodshop" size="30" style="height:30px;">
+        <button>検索</button>
+    </div>
+
+    <div class='cart'>
+        <a href='#'>
+            <img src="img/cart.png" alt="cart" while="50" height="50">
+        </a>
+    <div>
+
+    <div class='login'>
+        <a href="#">
+            <img src="img/account.png" alt="login" while="50" height="50">
+        </a>
+    </div>
+</header>
+</body>
+</html>
